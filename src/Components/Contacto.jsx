fa478956b84d39cbe8be50c2bac6f6467935a0b4
Mariana Amy <63118971+mmarianaamy@users.noreply.github.com>
2024-05-13 14:55:59 -0600

    Front

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Contacto() {
   return (
-    <div>Contacto</div>
+    <div class="contacto">
+      <div>
+        <h2>Contacto</h2>
+        <p>Escribenos y con gusto te ayudamos con las dudas que tengas</p>
+      </div>
+      <div>
+        
+      </div>
+    </div>
   )
 }
