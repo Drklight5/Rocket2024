e4ce540c2a0ef122d0319c947324eb0a24ec8c3e
Valeria <valeria050603@hotmail.com>
2024-05-13 14:02:47 -0600

    Avance Jiji

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import logo from '../Assets/logo-rocket.png'
+import { Link } from 'react-router-dom'
 
 export default function Menu() {
   return (
     <div className='Menu'>
-        <div className='logo'>
-          <img src={logo} alt="" />
-        </div>
-        <a className="link" href="">Desafíos</a>
-        <a className="link" href="">Beneficios</a>
-        <a className="link" href="">Evento</a>
-        <a className="link terminos" href="">Terminos y condiciones</a>
+    
+        <Link to="/" className='logo'><img src={logo} alt="" /></Link>
+        <Link to="Arca-Continental" className='link'>Arca Continental</Link>
+        <Link to="Desafios" className='link'>Desafíos</Link>
+        <Link to="Impacto" className='link'>Impacto</Link>
+        <Link to="Evento" className='link'>Evento</Link>
+        <Link to="Terminos" className='link terminos'>Términos y Condiciones</Link>
     </div>
   )
 }
